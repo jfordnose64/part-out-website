419a7aaeec4d7285d809f5e5c0ea282eb1f341c5
jackson ford <jfordnose64@gmail.com>
2020-10-14 20:23:41 -0400

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import About from '../src/pages/about/about.jsx'
+import Home from '../src/pages/home/home'
+import Header from '../src/components/header/header'
+import Footer from '../src/components/footer/footer'
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import PageNotFound from './pages/page-not-found/page-not-found.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="**">
+          <PageNotFound />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
