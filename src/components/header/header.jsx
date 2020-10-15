0bae0c03a199a4f7f436daf3680c7dede5e7da44
MasterFugazi <justin@codechimps.co>
2020-10-14 22:54:31 -0400

    Added inital header styles

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,9 +4,20 @@ import './header.css'
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>Header!</h1>
-      </div>
+      <header class="head">
+        <div id="nav" class="navigation-bar">
+          <h1 class="logo">PartShop<span>r</span></h1>
+          <nav>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Signup</a></li>
+          <li><a href="">Why Us</a></li>
+        </ul>
+          </nav>
+        </div>
+        <hr></hr>
+      </header>
+
     )
   }
 }
